54c3d98728d4e9f79e1686cbdc2c1ab6e8367d9b
Julie Newcomb <newcombj@cs.washington.edu>
2018-08-07 16:16:37 -0400

    create parsers for munging testcase files

---
+++ parsers/mask-constants.rkt
@@ -0,0 +1,75 @@
+#lang racket
+
+(require parser-tools/yacc
+         parser-tools/lex
+         (prefix-in : parser-tools/lex-sre))
+
+(require "parser.rkt")
+
+(define constants-hash (make-hash))
+
+(define (get-wild-constant h const)
+  (let ([count (length (hash-keys h))])
+    (hash-ref h const (λ () (let ([const-var (string-append "c" (number->string count))])
+                              (begin (hash-set! h const const-var)
+                                     const-var))))))
+
+(define string-with-masked-constants
+  (parser
+   (start start)
+   (end newline EOF)
+   (tokens value-tokens op-tokens)
+   (error (λ (a b c) (void)))
+   (precs (right EQ)
+          (left < > GE LE)
+          (left OR AND)
+          (left - +)
+          (left * / %)
+          (left NEG)
+          (left !))
+
+   (grammar
+
+    (start [() #f]
+           [(error start) $2]
+           [(exp) $1])
+
+    (exp [(NUM) (get-wild-constant constants-hash $1)]
+         [(VAR) $1]
+         [(MAX OP exp COMMA exp CP) (format "max(~a,~a)" $3 $5)]
+         [(MIN OP exp COMMA exp CP) (format "min(~a,~a)" $3 $5)]
+         [(! OP exp CP) (format "!(~a)" $3)]
+         [(SELECT OP exp COMMA exp COMMA exp CP) (format "select(~a,~a,~a)" $3 $5 $7)]
+         [(exp AND exp) (format "~a&&~a" $1 $3)]
+         [(exp OR exp) (format "~a||~a" $1 $3)]
+         [(exp + exp) (format "~a+~a" $1 $3)]
+         [(exp - exp) (format "~a-~a" $1 $3)]
+         [(exp * exp) (format "~a*~a" $1 $3)]
+         [(exp / exp) (format "~a/~a" $1 $3)]
+         [(exp % exp) (format "~a%~a" $1 $3)]
+         [(exp < exp) (format "~a<~a" $1 $3)]
+         [(exp > exp) (format "~a>~a" $1 $3)]
+         [(exp EQ exp) (format "~a==~a" $1 $3)]
+         [(exp GE exp) (format "~a>=~a" $1 $3)]
+         [(exp LE exp) (format "~a<=~a" $1 $3)]
+         [(- exp) (format "-~a" $2)]
+         [(OP exp CP) (format "(~a)" $2)]))))
+
+(define (masked-constants s)
+  (let ([ip (open-input-string s)])
+    (port-count-lines! ip)
+    (string-append (string-with-masked-constants (λ () (halide-lexer ip))) " ~~~ " s "\n")))
+
+(define (test-parsing in-file)
+  (for-each (λ (s) (begin
+                     (displayln s)
+                     (masked-constants s)))
+            (file->lines in-file)))
+
+(call-with-output-file "/Users/jnewcomb/testcases/08_02_2018/processed_part5.txt" #:exists 'append
+  (λ (out)
+    (for-each (λ (s) (begin
+                       (hash-clear! constants-hash)
+                       (write-string (masked-constants s) out)))
+              (file->lines "/Users/jnewcomb/testcases/08_02_2018/part5.txt"))))
+
--- parsers/parser.rkt
+++ parsers/parser.rkt
@@ -8,8 +8,10 @@
          parser-tools/lex
          (prefix-in : parser-tools/lex-sre))
 
+(provide halide-lexer value-tokens op-tokens)
+
 (define-tokens value-tokens (NUM VAR))
-(define-empty-tokens op-tokens (newline = OP CP COMMA + - * / ^ < > ! GE LE EOF NEG OR AND MAX MIN SELECT))
+(define-empty-tokens op-tokens (newline OP CP COMMA + - * / % ^ < > ! EQ GE LE EOF NEG OR AND MAX MIN SELECT))
 
 ;; A hash table to store variable values in for the calculator
 (define vars (make-hash))
@@ -21,19 +23,20 @@
 
  ;; (:/ 0 9) would not work because the lexer does not understand numbers.  (:/ #\0 #\9) is ok too.
  (digit (:/ "0" "9")))
- 
-(define calcl
+
+(define halide-lexer
   (lexer
    [(eof) 'EOF]
    ;; recursively call the lexer on the remaining input after a tab or space.  Returning the
    ;; result of that operation.  This effectively skips all whitespace.
-   [(:or #\tab #\space) (calcl input-port)]
+   [(:or #\tab #\space) (halide-lexer input-port)]
    ;; (token-newline) returns 'newline
    [#\newline (token-newline)]
    ;; Since (token-=) returns '=, just return the symbol directly
-   [(:or "=" "+" "-" "*" "/" "<" ">" "!") (string->symbol lexeme)]
+   [(:or "+" "-" "*" "/" "<" ">" "!" "%") (string->symbol lexeme)]
    [">=" 'GE]
    ["<=" 'LE]
+   ["==" 'EQ]
    ["(" 'OP]
    [")" 'CP]
    ["," 'COMMA]
@@ -42,10 +45,11 @@
    ["max" 'MAX]
    ["min" 'MIN]
    ["select" 'SELECT]
-   [(:+ (:or lower-letter upper-letter)) (token-VAR (string->symbol lexeme))]
+   ;[(:+ (:or lower-letter upper-letter)) (token-VAR (string->symbol lexeme))]
+   [(:: "v" (:+ digit)) (token-VAR (string->symbol lexeme))]
    [(:+ digit) (token-NUM (string->number lexeme))]
    [(:: (:+ digit) #\. (:* digit)) (token-NUM (string->number lexeme))]))
-   
+
 
 (define calcp
   (parser
@@ -55,11 +59,11 @@
    (tokens value-tokens op-tokens)
    (error (lambda (a b c) (void)))
 
-   (precs (right =)
+   (precs (right EQ)
           (left < > GE LE)
           (left OR AND)
           (left - +)
-          (left * /)
+          (left * / %)
           (left NEG)
           (left !))
    
@@ -73,8 +77,9 @@
     
     (exp [(NUM) $1]
          [(VAR) (hash-ref vars $1 (lambda () 0))]
-         [(VAR = exp) (begin (hash-set! vars $1 $3)
-                             $3)]
+        ; [(VAR = exp) (begin (hash-set! vars $1 $3)
+        ;                     $3)]
+         [(exp EQ exp) (equal? $1 $3)]
          [(MAX OP exp COMMA exp CP) (max $3 $5)]
          [(MIN OP exp COMMA exp CP) (min $3 $5)]
          [(! OP exp CP) (not $3)] ;; note that calc below doesn't print the value false
@@ -87,20 +92,22 @@
          [(exp / exp) (/ $1 $3)]
          [(exp < exp) (< $1 $3)]
          [(exp > exp) (> $1 $3)]
+         [(exp % exp) (modulo $1 $3)]
          [(exp GE exp) (>= $1 $3)]
          [(exp LE exp) (<= $1 $3)]
          [(- exp) (prec NEG) (- $2)]
          [(OP exp CP) $2]))))
+
            
 ;; run the calculator on the given input-port       
 (define (calc ip)
   (port-count-lines! ip)
   (letrec ((one-line
 	    (lambda ()
-	      (let ((result (calcp (lambda () (calcl ip)))))
+	      (let ((result (calcp (lambda () (halide-lexer ip)))))
 		(when result
                   (printf "~a\n" result)
                   (one-line))))))
     (one-line)))
 
-(calc (open-input-string "x=1\n(x + 2 * 3) - (1+2)*3"))
+;(calc (open-input-string "x=1\n(x + 2 * 3) - (1+2)*3"))
