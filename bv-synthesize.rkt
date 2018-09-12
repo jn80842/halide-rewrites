4cd7ed6051a7326bc7ce59ad580d314bec57c658
Julie Newcomb <newcombj@cs.washington.edu>
2018-09-12 15:13:16 -0400

    printing bitvector programs

--- bv-synthesize.rkt
+++ bv-synthesize.rkt
@@ -87,7 +87,41 @@
                                                       bvsge
                                                       (if (bveq idx (bv 13 bvw))
                                                           bveq
-                                                          noop)))))))))))))))
+                                                          (if (bveq idx (bv 14 bvw))
+                                                              bvnot-hld
+                                                              noop))))))))))))))))
+(define (get-operator-name-by-idx idx)
+  (if (bveq idx (bv 0 bvw))
+      "bvadd"
+      (if (bveq idx (bv 1 bvw))
+          "bvsub"
+          (if (bveq idx (bv 2 bvw))
+              "bvmul"
+              (if (bveq idx (bv 3 bvw))
+                  "bvsdiv"
+                  (if (bveq idx (bv 4 bvw))
+                      "bvsmod"
+                      (if (bveq idx (bv 5 bvw))
+                          "bvsmax"
+                          (if (bveq idx (bv 6 bvw))
+                              "bvsmin"
+                              (if (bveq idx (bv 7 bvw))
+                                  "bvand"
+                                  (if (bveq idx (bv 8 bvw))
+                                      "bvor"
+                                      (if (bveq idx (bv 9 bvw))
+                                          "bvslt"
+                                          (if (bveq idx (bv 10 bvw))
+                                              "bvsle"
+                                              (if (bveq idx (bv 11 bvw))
+                                                  "bvsgt"
+                                                  (if (bveq idx (bv 12 bvw))
+                                                      "bvsge"
+                                                      (if (bveq idx (bv 13 bvw))
+                                                          "bveq"
+                                                          (if (bveq idx (bv 14 bvw))
+                                                              "bvnot-hld"
+                                                              "noop"))))))))))))))))
 (define get-op get-operator-function-by-idx)
 
 (define (get-fixed-input idx i0 i1 i2 i3)
@@ -114,6 +148,24 @@
                           (if (bveq idx (bv 6 bvw))
                               (list-ref inputs 6)
                               (list-ref inputs 7)))))))))
+
+(define (get-fixed-input-names3 idx)
+  (let ([inputs (for/list ([i (range (expt 2 3))]) (string-append "b" (number->string i)))])
+    (if (bveq idx (bv 0 bvw))
+      (list-ref inputs 0)
+      (if (bveq idx (bv 1 bvw))
+          (list-ref inputs 1)
+          (if (bveq idx (bv 2 bvw))
+              (list-ref inputs 2)
+              (if (bveq idx (bv 3 bvw))
+                  (list-ref inputs 3)
+                  (if (bveq idx (bv 4 bvw))
+                      (list-ref inputs 4)
+                      (if (bveq idx (bv 5 bvw))
+                          (list-ref inputs 5)
+                          (if (bveq idx (bv 6 bvw))
+                              (list-ref inputs 6)
+                              (list-ref inputs 7))))))))))
 
 (struct fixedsk2 (op0 op1 op2 input0 input1 input2 input3) #:transparent)
 
@@ -146,6 +198,25 @@
                                                                                              (get-fixed-input3 (fixedsk3-idx5 fsk) inputs))
                                                                 ((get-op (fixedsk3-op6 fsk)) (get-fixed-input3 (fixedsk3-idx6 fsk) inputs)
                                                                                              (get-fixed-input3 (fixedsk3-idx7 fsk) inputs)))))))
+(define (get-string-sketch-function3 fsk)
+    (string-append "(define (synthesized-function "
+                   (string-join (for/list ([i (range (expt 2 3))]) (string-append "b" (number->string i))) " ")
+                   ")\n"
+                   "  (" (get-operator-name-by-idx (fixedsk3-op0 fsk)) " "
+                   "(" (get-operator-name-by-idx (fixedsk3-op1 fsk)) " "
+                   "(" (get-operator-name-by-idx (fixedsk3-op3 fsk)) " "
+                   (get-fixed-input-names3 (fixedsk3-idx0 fsk)) " "
+                   (get-fixed-input-names3 (fixedsk3-idx1 fsk)) ") "
+                   "(" (get-operator-name-by-idx (fixedsk3-op4 fsk)) " "
+                   (get-fixed-input-names3 (fixedsk3-idx2 fsk)) " "
+                   (get-fixed-input-names3 (fixedsk3-idx3 fsk)) ")) "
+                   "(" (get-operator-name-by-idx (fixedsk3-op2 fsk)) " "
+                   "(" (get-operator-name-by-idx (fixedsk3-op5 fsk)) " "
+                   (get-fixed-input-names3 (fixedsk3-idx4 fsk)) " "
+                   (get-fixed-input-names3 (fixedsk3-idx5 fsk)) ") "
+                   "(" (get-operator-name-by-idx (fixedsk3-op6 fsk)) " "
+                   (get-fixed-input-names3 (fixedsk3-idx6 fsk)) " "
+                   (get-fixed-input-names3 (fixedsk3-idx7 fsk)) "))))"))
 
 #;(define (synthesize-predicate expr sk nc-inputs const-inputs)
   (let* ([sym-consts (append (map (λ (i) (get-sym-bv)) (list-tail const-inputs 1)) (list (bv 0 bvw)))]
